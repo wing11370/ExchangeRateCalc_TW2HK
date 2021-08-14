efb76a6d429cc802b9f5c83659461d8cc0ff9d34
wing11370 <wing11370@gmail.com>
2021-08-15 01:25:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-import TodayUnionPayRate
+from TodayUnionPayRate_DBver import Flow_DBver
 if __name__ == "__main__":
-    TodayUnionPayRate.start()
+
+    newflow = Flow_DBver()
+    # newflow.flow_delete_data()
+    # newflow.flow_save()
+    # newflow.flow_check_data()
+    newflow.flow_run()
