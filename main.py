408137a38cb578635832b10740952781d331c060
wing11370 <wing11370@gmail.com>
2021-08-24 11:46:30 +0800

    add message

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ from TodayUnionPayRate_DBver import Flow_DBver
 if __name__ == "__main__":
 
     newflow = Flow_DBver()
+
+
+    # ======維護使用======
     # newflow.flow_delete_data()
-    # newflow.flow_save()
+    # newflow.flow_save() #每天執行一次
     # newflow.flow_check_data()
+
+    # ======執行使用======
     newflow.flow_run()
